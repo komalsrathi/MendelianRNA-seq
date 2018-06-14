c1f6229381bcd1f06e4a03cf786e73468f11f441
rathik <rathik@email.chop.edu>
2018-06-14 18:27:54 -0400

    updates scripts

--- Analysis/run_FilterSpliceJunctions.sh
+++ Analysis/run_FilterSpliceJunctions.sh
@@ -1,9 +1,13 @@
-python FilterSpliceJunctions.py -splice_file All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10.txt
+python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10.txt
 
-python FilterSpliceJunctions.py -splice_file All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -n_samples 1 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_Only1Sample.txt
+python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -n_samples 1 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_Only1Sample.txt
 
-python FilterSpliceJunctions.py -splice_file All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 5 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads5.txt
+python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 5 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads5.txt
 
-python FilterSpliceJunctions.py -splice_file All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 5 -add_OMIM > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads5_omim.txt
+python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 2 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads2.txt
 
-# python FilterSpliceJunctions.py -splice_file All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -sample_with_highest_normalized_read_support > Sample_HighestReadSupport.txt
+python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 3 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads3.txt
+
+python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 5 -add_OMIM > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads5_omim.txt
+
+# python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -sample_with_highest_normalized_read_support > Sample_HighestReadSupport.txt
