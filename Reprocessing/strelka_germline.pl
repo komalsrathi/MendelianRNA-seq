1acb27db061dd4823d324fd20db51ac0783d8db6
rathik <rathik@email.chop.edu>
2018-08-05 23:14:10 -0400

    adds strelka pipeline

--- Reprocessing/strelka_germline.pl
+++ Reprocessing/strelka_germline.pl
@@ -3,13 +3,13 @@ use Data::Dumper;
 use File::Slurp;
 use File::Basename;
 
-my @samples = read_file '/mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/MendelianRNA-seq/data/samples.txt';
+my @samples = read_file '/mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/MendelianRNA-seq/data/strelka_test_samples.txt';
 # my $bamdir = '/mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/strelka_out/bams';
 my $bamdir = '/mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/aligned/for-igv';
 my $fasta = '/mnt/isilon/cbmi/variome/reference/hg19_GTEx_v6p/Homo_sapiens_assembly19.fasta';
 # my $strelkabin = '/mnt/isilon/maris_lab/target_nbl_ngs/shared_resources/apps/strelka-2.9.2.centos6_x86_64/bin';
 my $strelkabin = '/mnt/isilon/maris_lab/target_nbl_ngs/shared_resources/apps/strelka-2.9.4.release_src/bin';
-my $strelkaout = '/mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/strelka_out';
+my $strelkaout = '/mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/strelka_out/deduped_test';
 
 # start time
 my $start_run = time();
