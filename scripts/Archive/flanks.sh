41f2db68c7ebf9819564b8f3bfe3dd425d943707
rathik <rathik@email.chop.edu>
2019-08-08 16:51:04 -0400

    remove unused scripts

--- scripts/Archive/flanks.sh
+++
@@ -1,3 +0,0 @@
-fetchChromSizes hg19 > hg19.chrom.sizes
-grep -v gene ../annotation/gencode.v19.cdl_genes.v7.patched_contigs.exons.txt | sed 's/^/chr/g' - > gencode.v19.cdl_genes.v7.patched_contigs.exons.bed
-bedtools flank -i gencode.v19.cdl_genes.v7.patched_contigs.exons.bed -g hg19.chrom.sizes -b 10 > gencode.v19.cdl_genes.v7.patched_contigs.exons_flanks.bed 
