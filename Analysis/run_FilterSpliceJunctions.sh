25e3d08b4eae3807bcb74335c269fdb86d0a9614
rathik <rathik@email.chop.edu>
2019-08-09 18:15:18 -0400

    update filter code

--- Analysis/run_FilterSpliceJunctions.sh
+++ Analysis/run_FilterSpliceJunctions.sh
@@ -1,13 +1,18 @@
+# Various ways to filter splice junctions
+# normalized values
 python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10.txt
 
+# normalized values, at least 10 read support across full dataset and only found in 1 sample 
 python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -n_samples 1 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_Only1Sample.txt
 
+# normalized values, at least 10 read support across full dataset, filter any sample with < 5 reads support, and only found in 1 sample 
 python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 5 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads5.txt
 
+# normalized values, at least 10 read support across full dataset, filter any sample with < 2 reads support, and only found in 1 sample 
 python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 2 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads2.txt
 
+# normalized values, at least 10 read support across full dataset, filter any sample with < 3 reads support, and only found in 1 sample 
 python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 3 > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads3.txt
 
+# normalized values, at least 10 read support across full dataset, filter any sample with < 5 reads support, only found in 1 sample and add OMIM annotation
 python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -filter_n_reads 5 -add_OMIM > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/FilteredJunctions_ReadSupport10_FilterReads5_omim.txt
-
-# python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -sample_with_highest_normalized_read_support > Sample_HighestReadSupport.txt
