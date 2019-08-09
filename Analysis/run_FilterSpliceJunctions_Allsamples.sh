2af5184b4475b634710562760557c9d59d1851dc
rathik <rathik@email.chop.edu>
2019-08-09 18:15:41 -0400

    update filter code

--- Analysis/run_FilterSpliceJunctions_Allsamples.sh
+++ Analysis/run_FilterSpliceJunctions_Allsamples.sh
@@ -1,3 +1,5 @@
+# Patient-specific splice junctions with highest read support and atleast 10 read support 
+# We chose this approach for the paper
 filename="/mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/MendelianRNA-seq/data/samples.txt"
 while read -r line
 do
@@ -5,5 +7,3 @@ do
     echo "$name"
     python FilterSpliceJunctions.py -print_simple -splice_file /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/All.CDL_genes.normalized.splicing.txt -n_read_support 10 -include_normalized -sample_with_highest_normalized_read_support $name > /mnt/isilon/cbmi/variome/rathik/mendelian_rnaseq/splice-events/$name"_HighestReadSupport.txt"
 done < "$filename"
-
-#
